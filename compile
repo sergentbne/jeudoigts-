7bf829eee3963bb10426a1c150d18cb7b94a402b
sergentbne <louis.turpault@gmail.com>
2022-08-11 07:09:08 +0000

    lol ok

---
+++ compile
@@ -0,0 +1,4 @@
+rm test
+echo "le fichier à été efface"
+g++ test.cpp -o test
+echo "fini de compiler"
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	string brother;
+	getline(cin,brother);
+	cout << brother << endl;
+}	
+	
