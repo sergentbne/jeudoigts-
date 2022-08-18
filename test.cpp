6e865072be54a55c72af47110672e97f7d91ffc9
Louis Turpault <louis.turpault@gmail.com>
2022-08-18 14:49:50 -0400

    did some shit

--- test.cpp
+++ test.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 #include <unistd.h>
 int main() {
-	sleep(5);
-	cout << "hey";
+	cout << "je vais assumer que vous connaissez le jeu des doigts" << endl;
+	cout << "comment se nomme le premier joueur?\n";
+	string premier-joueur;
+	getline(cin,premier-joueur);
+	cout << "comment se nomme le deuxieme joueur?\n";
+	string deuxieme-joueur;
+	getline(cin,deuxieme-joueur);
+	bool win;
+	struct {
+	int main0 = 1;
+	int main1 = 1;
+	string nom;
+	} j0,j2;
+	while (win == false); {
+	
+	}
 }	
 	
